7ff12338b9f5edaa08f08984e0462881a1b1b9bd
Rehankumar554 <Rehankumarsahu828@gmail.com>
2023-06-12 13:03:36 +0530

    first commit

---
+++ i2js.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".auto_input", {
+      strings: ["www.technicalyogi.in"],
+      typeSpeed: 30,
+      backSpeed: 30,
+      loop: false
+    });
+  });
+  
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".auto_input_ii", {
+      strings: ["Knowledgeable Post In", "Computer Category", "Smartphone Category", "Tips n Tricks", "Vs Code", "Games n hacks", "Etc."],
+      typeSpeed: 30,
+      backSpeed: 30,
+      loop: true
+    });
+  });
+  
---
+++ js.js
@@ -0,0 +1,33 @@
+br = document.querySelector(".br");
+navbar = document.querySelector(".navbar");
+navlist = document.querySelector(".nav-list");
+rightnav = document.querySelector(".rightnav");
+
+br.addEventListener("click", () => {
+  rightnav.classList.toggle("v-class");
+  navlist.classList.toggle("v-class");
+  navbar.classList.toggle("h-nav");
+});
+
+// Get the modal
+var modal = document.getElementById("id01");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// Get the modal
+var modal = document.getElementById("id02");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+;
+
+
